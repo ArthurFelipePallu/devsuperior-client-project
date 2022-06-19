7d44295fede3ab765e31181763613790bb450fac
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-06-19 19:49:23 -0300

    Delete Client

--- backend/src/main/java/com/devsuperior/client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/client/services/ClientService.java
@@ -7,12 +7,14 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.client.dtos.ClientDTO;
 import com.devsuperior.client.entities.Client;
 import com.devsuperior.client.repositories.ClientRepository;
+import com.devsuperior.client.services.exceptions.DatabaseException;
 import com.devsuperior.client.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -66,7 +68,18 @@ public class ClientService {
 		}		
 	}
 	
-	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+	}
 	
 	
 }
