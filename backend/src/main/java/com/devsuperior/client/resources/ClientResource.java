88ba4cf794369b4dcc7d09c9b2cced63f53649ce
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-06-19 19:30:57 -0300

    Client Get byID ,  Insert Client

--- backend/src/main/java/com/devsuperior/client/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/client/resources/ClientResource.java
@@ -1,12 +1,17 @@
 package com.devsuperior.client.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.client.dtos.ClientDTO;
 import com.devsuperior.client.services.ClientService;
@@ -24,4 +29,21 @@ public class ClientResource {
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}	
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id)
+	{
+		ClientDTO list = service.findById(id);
+		return ResponseEntity.ok().body(list);
+	}	
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto)
+	{
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);		
+	}	
+	
+	
 }
--- backend/src/main/java/com/devsuperior/client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/client/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.client.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.client.dtos.ClientDTO;
 import com.devsuperior.client.entities.Client;
 import com.devsuperior.client.repositories.ClientRepository;
+import com.devsuperior.client.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,4 +24,26 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x->new ClientDTO(x)).collect(Collectors.toList());		
 	}
+	
+	@Transactional(readOnly=true)
+	public ClientDTO findById(Long id){
+		Optional<Client> obj = repository.findById(id);
+		Client c = obj.orElseThrow(()->new ResourceNotFoundException("Entity NOT FOUND"));
+		return new ClientDTO(c);		
+	}
+
+	@Transactional(readOnly=true)
+	public ClientDTO insert(ClientDTO dto) {
+
+		Client client = new Client();
+		client.setName(dto.getName());
+		client.setBirthDate(dto.getBirthDate());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setChildren(dto.getChildren());
+		
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+	
 }
